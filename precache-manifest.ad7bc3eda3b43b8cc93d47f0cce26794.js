ce00b78f4ae8c1c95533ef0e5bca6fe55f2a16f0
Jonas Perez <00jonasperez@gmail.com>
2019-02-13 06:26:35 +0000

    added repeat button, fixed some styling issues

---
+++ precache-manifest.ad7bc3eda3b43b8cc93d47f0cce26794.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "74ba238178aeda50358b",
+    "url": "/js/chunk-vendors.b61b114d.js"
+  },
+  {
+    "revision": "16b8c0323eaa449c360a",
+    "url": "/js/app.05556f85.js"
+  },
+  {
+    "revision": "1537c53d14d1c53d7c2357810cb1011f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74ba238178aeda50358b",
+    "url": "/css/chunk-vendors.81d22ad2.css"
+  },
+  {
+    "revision": "16b8c0323eaa449c360a",
+    "url": "/css/app.7b13568f.css"
+  }
+];
